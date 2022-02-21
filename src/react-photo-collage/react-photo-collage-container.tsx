f9469dbeac86d838f80646f80df52094c8073037
Glen Maddern <glen@cloudflare.com>
2022-02-21 15:20:48 +0000

    this good now

--- src/react-photo-collage/react-photo-collage-container.tsx
+++ src/react-photo-collage/react-photo-collage-container.tsx
@@ -1,104 +1,124 @@
-import React, { useEffect, useRef, useState, useCallback, useMemo } from 'react';
-import Carousel, { Modal, ModalGateway } from "react-images";
+import React, { useEffect, useRef, useState, useCallback, useMemo } from 'react'
+import Carousel, { Modal, ModalGateway } from 'react-images'
+// import { Carousel } from 'react-responsive-carousel'
 
-import {
-    ReactPhotoCollageComponent,
-} from './react-photo-collage-component';
+// <Carousel selectedItem={currentImage}>
+//   {photos.map((photo) => (
+//     <img key={photo.source} src={photo.source} alt="Image" />
+//   ))}
+// </Carousel>
 
+import { ReactPhotoCollageComponent } from './react-photo-collage-component'
+import styled from 'styled-components'
+const ModalStyles = styled.div`
+  .react-images__view-image {
+    max-width: 95%;
+    max-height: 95vh;
+    margin-top: 5vh;
+  }
+`
 const createPhotoIds = (photos) => {
-    return photos.map((data, i) => {
-        return { ...data, id: i }
-    });
+  return photos.map((data, i) => {
+    return { ...data, id: i }
+  })
 }
 const createLayoutPhotoMaps = (layout, photos) => {
-    const newPhotos = createPhotoIds(photos);
-    const newMaps = {};
-    layout.reduce((accumulator, currentValue, currentIndex) => {
-        newMaps[currentIndex] = newPhotos.slice(accumulator, accumulator + currentValue);
-        return accumulator + currentValue;
-    }, 0);
+  const newPhotos = createPhotoIds(photos)
+  const newMaps = {}
+  layout.reduce((accumulator, currentValue, currentIndex) => {
+    newMaps[currentIndex] = newPhotos.slice(
+      accumulator,
+      accumulator + currentValue
+    )
+    return accumulator + currentValue
+  }, 0)
 
-    return newMaps;
-
+  return newMaps
 }
 
 interface ReactPhotoCollageContainerProps {
-    width?: string;
-    height?: Array<string>;
-    layout: Array<number>;
-    photos: Array<{ thumbnail: string, source: string }>;
-    showNumOfRemainingPhotos?: boolean
+  width?: string
+  height?: Array<string>
+  layout: Array<number>
+  photos: Array<{ thumbnail: string; source: string }>
+  showNumOfRemainingPhotos?: boolean
 }
 const checkProps = (props: ReactPhotoCollageContainerProps) => {
-    const defaultProps = {
-        width: '800px',
-        height: new Array(props.layout.length),
-        layout: [],
-        photos: [],
-        showNumOfRemainingPhotos: false
+  const defaultProps = {
+    width: '800px',
+    height: new Array(props.layout.length),
+    layout: [],
+    photos: [],
+    showNumOfRemainingPhotos: false,
+  }
+  const newProps = { ...defaultProps, ...props }
+  if (newProps.height.length < newProps.layout.length) {
+    for (let i = 0; i < newProps.layout.length; i++) {
+      newProps.height[i] = '200px'
     }
-    const newProps = { ...defaultProps, ...props };
-    if (newProps.height.length < newProps.layout.length) {
-        for (let i = 0; i < newProps.layout.length; i++) {
-            newProps.height[i] = '200px';
-        }
-    }
-    return newProps;
+  }
+  return newProps
 }
-const ReactPhotoCollageContainer: React.FC<ReactPhotoCollageContainerProps> = (props) => {
-    const currProps = useMemo(() => checkProps(props), [props]);
-    const { width, height, layout, photos, showNumOfRemainingPhotos } = currProps;
-    const layoutNum = layout.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    const remainingNum = photos.length - layoutNum;
-    const [allowRender, setAllowRender] = useState<boolean>(false);
-    const [layoutPhotoMaps, setLayoutPhotoMaps] = useState<any>({});
-    const [viewerIsOpen, setViewerIsOpen] = useState<boolean>(false);
-    const [currentImage, setCurrentImage] = useState<number>(0);
+const ReactPhotoCollageContainer: React.FC<ReactPhotoCollageContainerProps> = (
+  props
+) => {
+  const currProps = useMemo(() => checkProps(props), [props])
+  const { width, height, layout, photos, showNumOfRemainingPhotos } = currProps
+  const layoutNum = layout.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  )
+  const remainingNum = photos.length - layoutNum
+  const [allowRender, setAllowRender] = useState<boolean>(false)
+  const [layoutPhotoMaps, setLayoutPhotoMaps] = useState<any>({})
+  const [viewerIsOpen, setViewerIsOpen] = useState<boolean>(false)
+  const [currentImage, setCurrentImage] = useState<number>(0)
 
-    useEffect(() => {
-        setLayoutPhotoMaps(createLayoutPhotoMaps(layout, photos));
-    }, []);
-    useEffect(() => {
-        Object.keys(layoutPhotoMaps).length ? setAllowRender(true) : setAllowRender(false);
-    }, [layoutPhotoMaps]);
+  useEffect(() => {
+    setLayoutPhotoMaps(createLayoutPhotoMaps(layout, photos))
+  }, [])
+  useEffect(() => {
+    Object.keys(layoutPhotoMaps).length
+      ? setAllowRender(true)
+      : setAllowRender(false)
+  }, [layoutPhotoMaps])
 
-    const openLightbox = useCallback((id) => {
-        setCurrentImage(parseInt(id));
-        setViewerIsOpen(true);
-    }, []);
-    const closeLightbox = useCallback(() => {
-        setCurrentImage(0);
-        setViewerIsOpen(false);
-    }, []);
+  const openLightbox = useCallback((id) => {
+    setCurrentImage(parseInt(id))
+    setViewerIsOpen(true)
+  }, [])
+  const closeLightbox = useCallback(() => {
+    setCurrentImage(0)
+    setViewerIsOpen(false)
+  }, [])
 
-    if (allowRender) {
-        return (
-            <React.Fragment>
-                <ReactPhotoCollageComponent
-                    width={width}
-                    height={height}
-                    layout={layout}
-                    layoutPhotoMaps={layoutPhotoMaps}
-                    layoutNum={layoutNum}
-                    remainingNum={remainingNum}
-                    showNumOfRemainingPhotos={showNumOfRemainingPhotos}
-                    openLightbox={openLightbox}
-                />
-                <ModalGateway>
-                    {
-                        viewerIsOpen ?
-                            (
-                                <Modal onClose={closeLightbox}>
-                                    <Carousel views={photos} currentIndex={currentImage} />
-                                </Modal>
-                            ) : null
-                    }
-                </ModalGateway>
-            </React.Fragment>
-        );
-    }
+  if (allowRender) {
+    return (
+      <React.Fragment>
+        <ReactPhotoCollageComponent
+          width={width}
+          height={height}
+          layout={layout}
+          layoutPhotoMaps={layoutPhotoMaps}
+          layoutNum={layoutNum}
+          remainingNum={remainingNum}
+          showNumOfRemainingPhotos={showNumOfRemainingPhotos}
+          openLightbox={openLightbox}
+        />
+        <ModalGateway>
+          {viewerIsOpen ? (
+            <Modal onClose={closeLightbox}>
+              <ModalStyles>
+                <Carousel views={photos} currentIndex={currentImage} />
+              </ModalStyles>
+            </Modal>
+          ) : null}
+        </ModalGateway>
+      </React.Fragment>
+    )
+  }
 
-    return null;
+  return null
 }
 
-export default ReactPhotoCollageContainer;
+export default ReactPhotoCollageContainer
